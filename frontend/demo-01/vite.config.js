9dc14abf4de8d1ebd020ff3c18f7ec56067abc80
Luminall <140222795+Jason6987@users.noreply.github.com>
2025-11-26 12:22:07 +0800

    chore: 添加code-inspector-plugin开发依赖

--- frontend/demo-01/vite.config.js
+++ frontend/demo-01/vite.config.js
@@ -2,9 +2,16 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    codeInspectorPlugin({
+      bundler: "vite",
+      editor: "code", // 指定 IDE 为 vscode https://inspector.fe-dev.cn/api/basic.html#editor
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
