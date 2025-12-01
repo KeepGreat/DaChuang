201f285aebf61f2cb9a5b997b53102c46696586f
Luminall <140222795+Jason6987@users.noreply.github.com>
2025-12-01 21:15:57 +0800

    feat: 实现practice模块的practice类的接口方法及类型定义，添加分页类型Page

--- frontend/demo-01/src/utils/types.js
+++ frontend/demo-01/src/utils/types.js
@@ -9,6 +9,18 @@
  * @property {T|null} data - 响应数据，某些操作可能为null
  */
 
+/**
+ * 分页响应结构，MyBatis-Plus 的 Page<T> 的部分属性
+ * https://baomidou.com/plugins/pagination/
+ * @template T - 列表项的数据类型
+ * @typedef {Object} Page
+ * @property {T[]} records - 查询数据列表，List<T>
+ * @property {number} total - 查询列表总记录数
+ * @property {number} current - 当前页码，从 1 开始
+ * @property {number} size - 每页显示条数，默认 10
+ * @property {number} pages - 总页数，根据 total 和 size 自动计算得出
+ */
+
 // 使用ES模块的空导出，确保文件被识别为ES模块
 export { };
 
