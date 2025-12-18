723cb89a5668c20b5a2092902df60e25b8e05dca
Luminall <140222795+Jason6987@users.noreply.github.com>
2025-12-18 11:12:42 +0800

    refactor: 简化SCSS文件结构和导入方式

--- frontend/demo-01/src/main.js
+++ frontend/demo-01/src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './index.css'
-import { pinia } from './store'
+import "@/styles/index.scss";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import { pinia } from "./store";
 
-const app = createApp(App)
-app.use(pinia)
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
