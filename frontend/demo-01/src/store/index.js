7437e85fa9790d3581d56ffb103b1dd9a587b152
Luminall <140222795+Jason6987@users.noreply.github.com>
2025-12-21 16:37:18 +0800

    refactor: 重构store导出，删除冗余导入

--- frontend/demo-01/src/store/index.js
+++ frontend/demo-01/src/store/index.js
@@ -1,18 +1,6 @@
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-// 导入所有store模块
-import { useAssignmentStore } from "./modules/assignmentStore";
-import { useCourseStore } from "./modules/courseStore";
-import { useDiscussionStore } from "./modules/discussionStore";
-import { useExamStore } from "./modules/examStore";
-import { useQuestionsStore } from "./modules/questionsStore";
-import { useResourceStore } from "./modules/resourceStore";
-import { useTaskStore } from "./modules/taskStore";
-import { useTeachingStore } from "./modules/teachingStore";
-import { testClassifm } from "./modules/testClassifm";
-import { useCounterStore } from "./modules/testCountStore";
-
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
@@ -28,23 +16,8 @@ export * from "./modules/practice/questionsStore";
 export * from "./modules/practice/userAnswerStore";
 export * from "./modules/resourceStore";
 export * from "./modules/taskStore";
+export * from "./modules/teachingStore";
 export * from "./modules/testClassifm";
 export * from "./modules/testCountStore";
 
 export { pinia };
-
-	export {
-		testClassifm,
-		useAssignmentStore,
-		// 原有的store
-		useCounterStore,
-		// 新增的store
-		useCourseStore,
-		useDiscussionStore,
-		useExamStore,
-		useQuestionsStore,
-		useResourceStore,
-		useTaskStore,
-		useTeachingStore
-	};
-
